2c04ccc81c777152196d7469adbe2ddf8960d05f
maduiep <emmanuelmaduip@gmail.com>
2020-06-19 05:41:34 +0100

    adding contents for our first recipe

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -7,8 +7,19 @@ class RecipeApp extends Component {
     return (
       <div className="App">
         <Recipe
-          title="Pasta"
-          ingredients={['Floor', 'Water']}
+          title="Nigerian Shawarma"
+          ingredients={[
+            '2 Chicken breasts(de-boned/boneless)',
+            '6+ Shawarma bread(either pita bread/flour tortilla wraps)',
+            '1 small sized Cabbage(cut into thin strips)',
+            '3 medium sized Carrots(grated or cut into thin strips)',
+            '1 large Cucumber(cut into thin strips&circles)',
+            '2 big Tomatoes(cut into thin strips)',
+            '1 medium sized Onions(cut into thin strips)',
+            'Ketchup',
+            'Mayonnaise',
+            '1 Tablespoon Vegetable /olive oil(for stir frying)'
+          ]}
           instructions="Mix ingredients"
           img="dinner.jpg"
         />
